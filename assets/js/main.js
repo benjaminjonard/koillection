e529cf7076659f703c011a6d5c12bbebb4bb1946
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2018-09-10 14:28:49 +0200

    Update to materialize v1.0.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,9 +44,33 @@ $(document).ready(function() {
     });
 
     //Init MaterializeCSS selects
-    $('select').material_select();
+    M.FormSelect.init(document.querySelectorAll('select'));
+
     //Init MaterializeCSS modals
-    $('.modal').modal();
+    M.Modal.init(document.querySelectorAll('.modal'));
+
+    //Init MaterializeCSS datepickers
+    M.Datepicker.init(document.querySelectorAll('.datepicker'), {
+        monthsFull: [Translator.trans('global.months.january'), Translator.trans('global.months.february'),
+            Translator.trans('global.months.march'), Translator.trans('global.months.april'), Translator.trans('global.months.may'),
+            Translator.trans('global.months.june'), Translator.trans('global.months.july'), Translator.trans('global.months.august'),
+            Translator.trans('global.months.september'), Translator.trans('global.months.october'), Translator.trans('global.months.november'), Translator.trans('global.months.december')],
+        monthsShort: [Translator.trans('global.months.january').substring(0, 3), Translator.trans('global.months.february').substring(0, 3),
+            Translator.trans('global.months.march').substring(0, 3), Translator.trans('global.months.april').substring(0, 3), Translator.trans('global.months.may').substring(0, 3),
+            Translator.trans('global.months.june').substring(0, 3), Translator.trans('global.months.july').substring(0, 3), Translator.trans('global.months.august').substring(0, 3),
+            Translator.trans('global.months.september').substring(0, 3), Translator.trans('global.months.october').substring(0, 3), Translator.trans('global.months.november').substring(0, 3),
+            Translator.trans('global.months.december').substring(0, 3)],
+        weekdaysFull: [Translator.trans('global.days.sunday'), Translator.trans('global.days.monday'), Translator.trans('global.days.tuesday'), Translator.trans('global.days.wednesday'),
+            Translator.trans('global.days.thursday'), Translator.trans('global.days.friday'), Translator.trans('global.days.saturday')],
+        weekdaysLetter: [Translator.trans('global.days.sunday').substring(0, 1), Translator.trans('global.days.monday').substring(0, 1), Translator.trans('global.days.tuesday').substring(0, 1),
+            Translator.trans('global.days.wednesday').substring(0, 1), Translator.trans('global.days.thursday').substring(0, 1), Translator.trans('global.days.friday').substring(0, 1),
+            Translator.trans('global.days.saturday').substring(0, 1)],
+        today: Translator.trans('global.today').substring(0, 3)+'.',
+        clear: Translator.trans('btn.clear'),
+        close: Translator.trans('btn.close'),
+        format: 'dd/mm/yyyy',
+        formatSubmit: 'yyyy-mm-dd'
+    });
 
     //Init tabs
     $('.tab').click(function () {
@@ -54,30 +78,6 @@ $(document).ready(function() {
         $(this).addClass('current');
         $('.panel').addClass('hidden');
         $('#' + $(this).attr('for')).removeClass('hidden');
-    });
-
-    //Init MaterializeCSS datepickers
-    $('.datepicker').pickadate({
-        monthsFull: [Translator.trans('global.months.january'), Translator.trans('global.months.february'),
-                     Translator.trans('global.months.march'), Translator.trans('global.months.april'), Translator.trans('global.months.may'),
-                     Translator.trans('global.months.june'), Translator.trans('global.months.july'), Translator.trans('global.months.august'),
-                     Translator.trans('global.months.september'), Translator.trans('global.months.october'), Translator.trans('global.months.november'), Translator.trans('global.months.december')],
-        monthsShort: [Translator.trans('global.months.january').substring(0, 3), Translator.trans('global.months.february').substring(0, 3),
-                     Translator.trans('global.months.march').substring(0, 3), Translator.trans('global.months.april').substring(0, 3), Translator.trans('global.months.may').substring(0, 3),
-                     Translator.trans('global.months.june').substring(0, 3), Translator.trans('global.months.july').substring(0, 3), Translator.trans('global.months.august').substring(0, 3),
-                     Translator.trans('global.months.september').substring(0, 3), Translator.trans('global.months.october').substring(0, 3), Translator.trans('global.months.november').substring(0, 3),
-                     Translator.trans('global.months.december').substring(0, 3)],
-        weekdaysFull: [Translator.trans('global.days.sunday'), Translator.trans('global.days.monday'), Translator.trans('global.days.tuesday'), Translator.trans('global.days.wednesday'),
-                       Translator.trans('global.days.thursday'), Translator.trans('global.days.friday'), Translator.trans('global.days.saturday')],
-        weekdaysLetter: [Translator.trans('global.days.sunday').substring(0, 1), Translator.trans('global.days.monday').substring(0, 1), Translator.trans('global.days.tuesday').substring(0, 1),
-                       Translator.trans('global.days.wednesday').substring(0, 1), Translator.trans('global.days.thursday').substring(0, 1), Translator.trans('global.days.friday').substring(0, 1),
-                       Translator.trans('global.days.saturday').substring(0, 1)],
-        today: Translator.trans('global.today').substring(0, 3)+'.',
-        clear: Translator.trans('btn.clear'),
-        close: Translator.trans('btn.close'),
-        format: 'dd/mm/yyyy',
-        formatSubmit: 'yyyy-mm-dd'
-
     });
 
     //Init lightboxes
@@ -130,7 +130,7 @@ $(document).ready(function() {
         });
         $newLinkDiv.before($newForm);
         indexFieldTemplate++;
-        $('select').material_select();
+        M.FormSelect.init(document.querySelectorAll('select'));
         computePositions($collectionHolder);
         reloadSortableList($collectionHolder, '.field');
     }
