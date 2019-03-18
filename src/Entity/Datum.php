8bdb9eddada91c6d4eacaf6342a161127c31638a
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2019-03-18 11:25:53 +0100

    Fix migration

--- src/Entity/Datum.php
+++ src/Entity/Datum.php
@@ -46,7 +46,7 @@ class Datum implements LoggableInterface
 
     /**
      * @var string
-     * @ORM\Column(type="text", nullable=false)
+     * @ORM\Column(type="text", nullable=true)
      */
     private $value;
 
