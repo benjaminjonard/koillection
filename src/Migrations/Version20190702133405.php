f46dc41e16a78047f6d139d78614c2fab8a789b9
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2019-07-02 15:59:07 +0200

    Fix migration

--- src/Migrations/Version20190702133405.php
+++ src/Migrations/Version20190702133405.php
@@ -7,19 +7,15 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20190702133405 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Add `idx_visibility` index to `koi_collection`.';
     }
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE INDEX idx_visibility ON koi_collection (visibility)');
@@ -27,6 +23,6 @@ final class Version20190702133405 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-
+        $this->abortIf(true, 'Always move forward.');
     }
 }
