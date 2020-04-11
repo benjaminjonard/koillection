727a6b2af1eeb884ea2037ba34f52f124c586d16
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2020-04-11 10:18:34 +0200

    Move to PHP 7.4

--- src/Enum/LocaleEnum.php
+++ src/Enum/LocaleEnum.php
@@ -11,21 +11,21 @@ namespace App\Enum;
  */
 class LocaleEnum
 {
-    public const LOCALE_GB = 'gb';
+    public const LOCALE_EN = 'en';
     public const LOCALE_FR = 'fr';
 
     public const LOCALES = [
-        self::LOCALE_GB,
+        self::LOCALE_EN,
         self::LOCALE_FR
     ];
 
     public const FULL_LOCALES = [
-        self::LOCALE_GB => 'en_GB',
+        self::LOCALE_EN => 'en_GB',
         self::LOCALE_FR => 'fr_FR'
     ];
 
     public const LOCALES_TRANS_KEYS = [
-        self::LOCALE_GB => 'english',
+        self::LOCALE_EN => 'english',
         self::LOCALE_FR => 'french'
     ];
 
