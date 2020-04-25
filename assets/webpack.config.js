2fb612c7387948356fb76e184a63ae05350da639
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2020-04-25 13:21:53 +0200

    Improve dark theme

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -11,7 +11,7 @@ Encore
     .addStyleEntry('css/app', './css/app.css')
     .addStyleEntry('css/themes/sunset', './css/themes/sunset.css')
     .addStyleEntry('css/themes/teal', './css/themes/teal.css')
-    .addStyleEntry('css/themes/dark_mode', './css/themes/dark_mode.css')
+    .addStyleEntry('css/themes/dark', './css/themes/dark.css')
     .addStyleEntry('css/export', './css/export.css')
     .addStyleEntry('css/translation', './css/translation.css')
     .addStyleEntry('css/flags', './css/flags.css')
