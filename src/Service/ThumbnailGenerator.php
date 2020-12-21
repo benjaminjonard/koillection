8b6d9acb2fd3536485801ed59792a3bebd035e27
Benjamin Jonard <jonard.benjamin@protonmail.com>
2020-12-21 19:59:55 +0100

    Properly handle gif thumbnails

--- src/Service/ThumbnailGenerator.php
+++ src/Service/ThumbnailGenerator.php
@@ -6,6 +6,13 @@ namespace App\Service;
 
 class ThumbnailGenerator
 {
+    private GifResizer $gifResizer;
+
+    public function __construct(GifResizer $gifResizer)
+    {
+        $this->gifResizer = $gifResizer;
+    }
+
     /**
      * @param string $path
      * @param string $thumbnailPath
@@ -21,43 +28,13 @@ class ThumbnailGenerator
 
         list($width, $height, $mime) = getimagesize($path);
         $originalSize = filesize($path);
+        $thumbnailHeight = (int) floor($height * ($thumbnailWidth / $width));
 
         if ($width <= $thumbnailWidth) {
             return false;
         }
 
-        switch ($mime) {
-            case IMAGETYPE_GIF:
-                $image = imagecreatefromgif($path);
-                break;
-            case IMAGETYPE_JPEG:
-            case IMAGETYPE_JPEG2000:
-                $image = imagecreatefromjpeg($path);
-                break;
-            case IMAGETYPE_PNG:
-                $image = imagecreatefrompng($path);
-                break;
-            case IMAGETYPE_WEBP:
-                $image = imagecreatefromwebp($path);
-                break;
-            default:
-                throw new \Exception('Your image cannot be processed, please use another one.');
-        }
-
-        $thumbnailHeight = (int) floor($height * ($thumbnailWidth / $width));
-        $thumbnail = imagecreatetruecolor($thumbnailWidth, $thumbnailHeight);
-
-        //Transparency
-        if ($mime === IMAGETYPE_PNG || $mime === IMAGETYPE_WEBP) {
-            imagecolortransparent($thumbnail, imagecolorallocate($thumbnail, 0, 0, 0));
-            imagealphablending($thumbnail, false);
-            imagesavealpha($thumbnail, true);
-        } elseif ($mime === IMAGETYPE_GIF) {
-            imagecolortransparent($thumbnail, imagecolorallocate($thumbnail, 0, 0, 0));
-        }
-
-        imagecopyresampled($thumbnail, $image, 0, 0, 0, 0, $thumbnailWidth, $thumbnailHeight, $width, $height);
-
+        // Create user directory in uploads
         $dir = explode('/', $thumbnailPath);
         \array_pop($dir);
         $dir = implode('/', $dir);
@@ -66,22 +43,50 @@ class ThumbnailGenerator
             throw new \Exception('There was a problem while uploading the image. Please try again!');
         }
 
-        switch ($mime) {
-            case IMAGETYPE_GIF:
-                imagegif($thumbnail, $thumbnailPath);
-                break;
-            case IMAGETYPE_JPEG:
-            case IMAGETYPE_JPEG2000:
-                imagejpeg($thumbnail, $thumbnailPath, 100);
-                break;
-            case IMAGETYPE_PNG:
-                imagepng($thumbnail, $thumbnailPath);
-                break;
-            case IMAGETYPE_WEBP:
-                imagewebp($thumbnail, $thumbnailPath, 100);
-                break;
-            default:
-                break;
+        if ($mime === IMAGETYPE_GIF) {
+            $this->gifResizer->resize($path, $thumbnailPath, $thumbnailWidth, $thumbnailHeight);
+        } else {
+            switch ($mime) {
+                case IMAGETYPE_JPEG:
+                case IMAGETYPE_JPEG2000:
+                    $image = imagecreatefromjpeg($path);
+                    break;
+                case IMAGETYPE_PNG:
+                    $image = imagecreatefrompng($path);
+                    break;
+                case IMAGETYPE_WEBP:
+                    $image = imagecreatefromwebp($path);
+                    break;
+                default:
+                    throw new \Exception('Your image cannot be processed, please use another one.');
+            }
+
+
+            $thumbnail = imagecreatetruecolor($thumbnailWidth, $thumbnailHeight);
+
+            //Transparency
+            if ($mime === IMAGETYPE_PNG || $mime === IMAGETYPE_WEBP) {
+                imagecolortransparent($thumbnail, imagecolorallocate($thumbnail, 0, 0, 0));
+                imagealphablending($thumbnail, false);
+                imagesavealpha($thumbnail, true);
+            }
+
+            imagecopyresampled($thumbnail, $image, 0, 0, 0, 0, $thumbnailWidth, $thumbnailHeight, $width, $height);
+
+            switch ($mime) {
+                case IMAGETYPE_JPEG:
+                case IMAGETYPE_JPEG2000:
+                    imagejpeg($thumbnail, $thumbnailPath, 100);
+                    break;
+                case IMAGETYPE_PNG:
+                    imagepng($thumbnail, $thumbnailPath);
+                    break;
+                case IMAGETYPE_WEBP:
+                    imagewebp($thumbnail, $thumbnailPath, 100);
+                    break;
+                default:
+                    break;
+            }
         }
 
         $thumbnailSize = filesize($thumbnailPath);
