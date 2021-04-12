92528d02f08346bd32ffb323e8837c491b7a1a1f
benjamin.jonard <benjamin.jonard@sooyoos.com>
2021-04-12 15:11:16 +0200

    Migrate suggestions to stimulus

--- assets/js/collection.js
+++
@@ -1,4 +0,0 @@
-$('.suggestion').on('click', function () {
-    $(this).closest('.input-field').find('input').first().val($(this).data('suggestion'));
-    $(this).closest('.input-field').find('label').first().addClass('active');
-});
