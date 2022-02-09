092a24093e39e79f39b557183aa6a574f6f12611
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2022-02-09 18:35:30 +0100

    Start moving tests

--- tests/Api/Album/AlbumCurrentUserTest.php
+++ tests/Api/Album/AlbumCurrentUserTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Api;
+namespace App\Tests\Api\Album;
 
 use Api\Tests\AuthenticatedTest;
 use App\Entity\Album;
@@ -8,7 +8,7 @@ use App\Entity\Photo;
 use Doctrine\Common\Collections\Criteria;
 use Symfony\Component\HttpFoundation\Response;
 
-class AlbumTest extends AuthenticatedTest
+class AlbumCurrentUserTest extends AuthenticatedTest
 {
     public function testGetAlbums(): void
     {
@@ -21,7 +21,6 @@ class AlbumTest extends AuthenticatedTest
         $this->assertMatchesResourceCollectionJsonSchema(Album::class);
     }
 
-    // Interacting with current User's albums
     public function testGetAlbum(): void
     {
         $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->user], [], 1)[0];
@@ -124,96 +123,5 @@ class AlbumTest extends AuthenticatedTest
         $this->createClientWithCredentials()->request('DELETE', $iri);
 
         $this->assertResponseStatusCodeSame(Response::HTTP_NO_CONTENT);
-    }
-
-    // Interacting with another User's albums
-    public function testCantGetAnotherUserAlbum(): void
-    {
-        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
-        $iri = $this->iriConverter->getIriFromItem($album);
-
-        $this->createClientWithCredentials()->request('GET', $iri);
-        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
-    }
-
-    public function testCantGetAnotherUserAlbumChildren(): void
-    {
-        $criteria = (new Criteria())
-            ->where(Criteria::expr()->neq('parent', null))
-            ->andWhere(Criteria::expr()->eq('owner', $this->otherUser))
-        ;
-        $album = $this->em->getRepository(Album::class)->matching($criteria)[0]->getParent();
-        $iri = $this->iriConverter->getIriFromItem($album);
-
-        $response = $this->createClientWithCredentials()->request('GET', $iri . '/childrens');
-        $data = $response->toArray();
-
-        $this->assertResponseIsSuccessful();
-        $this->assertEquals(0, $data['hydra:totalItems']);
-        $this->assertCount(0, $data['hydra:member']);
-        $this->assertMatchesResourceCollectionJsonSchema(Album::class);
-    }
-
-    public function testCantGetAnotherUserAlbumParent(): void
-    {
-        $criteria = (new Criteria())
-            ->where(Criteria::expr()->neq('parent', null))
-            ->andWhere(Criteria::expr()->eq('owner', $this->otherUser))
-        ;
-        $album = $this->em->getRepository(Album::class)->matching($criteria)[0];
-        $iri = $this->iriConverter->getIriFromItem($album);
-
-        $this->createClientWithCredentials()->request('GET', $iri . '/parent');
-        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
-    }
-
-    public function testCantGetAnotherUserAlbumPhotos(): void
-    {
-        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
-        $iri = $this->iriConverter->getIriFromItem($album);
-
-        $response = $this->createClientWithCredentials()->request('GET', $iri . '/photos');
-        $data = $response->toArray();
-
-        $this->assertResponseIsSuccessful();
-        $this->assertEquals(0, $data['hydra:totalItems']);
-        $this->assertCount(0, $data['hydra:member']);
-        $this->assertMatchesResourceCollectionJsonSchema(Photo::class);
-    }
-
-    public function testCantPutAnotherUserAlbum(): void
-    {
-        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
-        $iri = $this->iriConverter->getIriFromItem($album);
-
-        $this->createClientWithCredentials()->request('PUT', $iri, ['json' => [
-            'title' => 'updated title with PUT',
-        ]]);
-
-        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
-    }
-
-    public function testCantPatchAnotherUserAlbum(): void
-    {
-        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
-        $iri = $this->iriConverter->getIriFromItem($album);
-
-        $this->createClientWithCredentials()->request('PATCH', $iri, [
-            'headers' => ['Content-Type: application/merge-patch+json'],
-            'json' => [
-                'title' => 'updated title with PATCH',
-            ]
-        ]);
-
-        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
-    }
-
-    public function testCantDeleteAnotherUserAlbum(): void
-    {
-        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
-        $iri = $this->iriConverter->getIriFromItem($album);
-        $this->createClientWithCredentials()->request('DELETE', $iri);
-
-        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
     }
 }
---
+++ tests/Api/Album/AlbumOtherUserTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Tests\Api\Album;
+
+use Api\Tests\AuthenticatedTest;
+use App\Entity\Album;
+use App\Entity\Photo;
+use Doctrine\Common\Collections\Criteria;
+use Symfony\Component\HttpFoundation\Response;
+
+class AlbumOtherUserTest extends AuthenticatedTest
+{
+    public function testCantGetAnotherUserAlbum(): void
+    {
+        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
+        $iri = $this->iriConverter->getIriFromItem($album);
+
+        $this->createClientWithCredentials()->request('GET', $iri);
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    public function testCantGetAnotherUserAlbumChildren(): void
+    {
+        $criteria = (new Criteria())
+            ->where(Criteria::expr()->neq('parent', null))
+            ->andWhere(Criteria::expr()->eq('owner', $this->otherUser))
+        ;
+        $album = $this->em->getRepository(Album::class)->matching($criteria)[0]->getParent();
+        $iri = $this->iriConverter->getIriFromItem($album);
+
+        $response = $this->createClientWithCredentials()->request('GET', $iri . '/childrens');
+        $data = $response->toArray();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(0, $data['hydra:totalItems']);
+        $this->assertCount(0, $data['hydra:member']);
+        $this->assertMatchesResourceCollectionJsonSchema(Album::class);
+    }
+
+    public function testCantGetAnotherUserAlbumParent(): void
+    {
+        $criteria = (new Criteria())
+            ->where(Criteria::expr()->neq('parent', null))
+            ->andWhere(Criteria::expr()->eq('owner', $this->otherUser))
+        ;
+        $album = $this->em->getRepository(Album::class)->matching($criteria)[0];
+        $iri = $this->iriConverter->getIriFromItem($album);
+
+        $this->createClientWithCredentials()->request('GET', $iri . '/parent');
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    public function testCantGetAnotherUserAlbumPhotos(): void
+    {
+        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
+        $iri = $this->iriConverter->getIriFromItem($album);
+
+        $response = $this->createClientWithCredentials()->request('GET', $iri . '/photos');
+        $data = $response->toArray();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(0, $data['hydra:totalItems']);
+        $this->assertCount(0, $data['hydra:member']);
+        $this->assertMatchesResourceCollectionJsonSchema(Photo::class);
+    }
+
+    public function testCantPutAnotherUserAlbum(): void
+    {
+        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
+        $iri = $this->iriConverter->getIriFromItem($album);
+
+        $this->createClientWithCredentials()->request('PUT', $iri, ['json' => [
+            'title' => 'updated title with PUT',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    public function testCantPatchAnotherUserAlbum(): void
+    {
+        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
+        $iri = $this->iriConverter->getIriFromItem($album);
+
+        $this->createClientWithCredentials()->request('PATCH', $iri, [
+            'headers' => ['Content-Type: application/merge-patch+json'],
+            'json' => [
+                'title' => 'updated title with PATCH',
+            ]
+        ]);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    public function testCantDeleteAnotherUserAlbum(): void
+    {
+        $album = $this->em->getRepository(Album::class)->findBy(['owner' => $this->otherUser], [], 1)[0];
+        $iri = $this->iriConverter->getIriFromItem($album);
+        $this->createClientWithCredentials()->request('DELETE', $iri);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+}
