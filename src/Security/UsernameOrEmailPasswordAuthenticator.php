55d3bb32d42668af79346942bc664f82d27563dc
benjaminjonard <jonard.benjamin@protonmail.com>
2022-06-24 09:54:58 +0200

    Fix zip exports in user uploads directory doesn't exist

--- src/Security/UsernameOrEmailPasswordAuthenticator.php
+++ src/Security/UsernameOrEmailPasswordAuthenticator.php
@@ -13,7 +13,6 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
