65b811e1c9c7ee75b2a8c0c4b7737781050ae14c
crishnakh <crishnakh@gmail.com>
2022-08-15 19:44:09 +0000

    Añado idioma a la lista

--- src/Enum/LocaleEnum.php
+++ src/Enum/LocaleEnum.php
@@ -8,10 +8,12 @@ class LocaleEnum
 {
     public const LOCALE_EN_GB = 'en-GB';
     public const LOCALE_FR_FR = 'fr-FR';
+    public const LOCALE_ES_ES = 'es-ES';
 
     public const LOCALES = [
         self::LOCALE_EN_GB => 'en-GB',
         self::LOCALE_FR_FR => 'fr-FR',
+        self::LOCALE_ES_ES => 'es-ES',
     ];
 
     public static function getLocaleLabels(): array
@@ -19,6 +21,7 @@ class LocaleEnum
         return [
             self::LOCALE_EN_GB => 'global.locale.en-GB',
             self::LOCALE_FR_FR => 'global.locale.fr-FR',
+            self::LOCALE_ES_ES => 'global.locale.es-ES',
         ];
     }
 }
