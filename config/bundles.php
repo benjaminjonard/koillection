49e537e2f19b068229dcd0a035fe96fa66158e61
benjaminjonard <benjamin.jonard@sooyoos.com>
2022-08-23 11:20:59 +0200

    Add php-translations in dev env, start cleaning translation files

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Translation\Bundle\TranslationBundle::class => ['dev' => true],
 ];
