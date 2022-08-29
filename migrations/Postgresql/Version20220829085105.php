721dd2cf9e4be8713be281ae98c9cd3d6b584713
benjaminjonard <benjamin.jonard@sooyoos.com>
2022-08-29 11:18:18 +0200

    Add migration for new locale values

---
+++ migrations/Postgresql/Version20220829085105.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations\Postgresql;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220829085105 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '[Postgresql] Update locale values';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->skipIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('UPDATE koi_user SET locale = \'en\' WHERE locale = \'en-GB\';');
+        $this->addSql('UPDATE koi_user SET locale = \'fr\' WHERE locale = \'fr-FR\';');
+        $this->addSql('UPDATE koi_user SET locale = \'es\' WHERE locale = \'es-ES\';');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->skipIf(true, 'Always move forward.');
+    }
+}
