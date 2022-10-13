7699fbbfda1351b6243c6ea5500528a7d9536f6c
benjaminjonard <benjamin.jonard@sooyoos.com>
2022-10-13 11:08:02 +0200

    More tests

---
+++ tests/Api/Loan/LoanApiNotOwnerTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Api\Loan;
+
+use Api\Tests\ApiTestCase;
+use App\Entity\Datum;
+use App\Entity\Item;
+use App\Entity\Loan;
+use App\Entity\Tag;
+use App\Factory\CollectionFactory;
+use App\Factory\DatumFactory;
+use App\Factory\ItemFactory;
+use App\Factory\LoanFactory;
+use App\Factory\TagFactory;
+use App\Factory\UserFactory;
+use Symfony\Component\HttpFoundation\Response;
+use Zenstruck\Foundry\Test\Factories;
+
+class LoanApiNotOwnerTest extends ApiTestCase
+{
+    use Factories;
+
+    public function test_cant_get_another_user_loan(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne()->object();
+        $owner = UserFactory::createOne()->object();
+        $collection = CollectionFactory::createOne(['owner' => $owner]);
+        $item = ItemFactory::createOne(['collection' => $collection, 'owner' => $owner]);
+        $loan = LoanFactory::createOne(['item' => $item, 'owner' => $owner]);
+
+        // Act
+        $this->createClientWithCredentials($user)->request('GET', '/api/loans/' . $loan->getId());
+
+        // Assert
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    public function test_cant_post_loan_with_another_user_item(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne()->object();
+        $owner = UserFactory::createOne()->object();
+        $collection = CollectionFactory::createOne(['owner' => $owner]);
+        $item = ItemFactory::createOne(['collection' => $collection, 'owner' => $owner]);
+
+        // Act
+        $this->createClientWithCredentials($user)->request('POST', '/api/loans/', ['json' => [
+            'lentTo' => 'Someone',
+            'lentAt' => '2022-10-01T12:00:00+02:00',
+            'item' => '/api/items/' . $item->getId()
+        ]]);
+
+        // Assert
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    public function test_cant_put_another_user_loan(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne()->object();
+        $owner = UserFactory::createOne()->object();
+        $collection = CollectionFactory::createOne(['owner' => $owner]);
+        $item = ItemFactory::createOne(['collection' => $collection, 'owner' => $owner]);
+        $loan = LoanFactory::createOne(['item' => $item, 'owner' => $owner]);
+
+        // Act
+        $this->createClientWithCredentials($user)->request('PUT', '/api/loans/'.$loan->getId(), ['json' => [
+            'lentTo' => 'Someone else',
+        ]]);
+
+        // Assert
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    public function test_cant_patch_another_user_loan(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne()->object();
+        $owner = UserFactory::createOne()->object();
+        $collection = CollectionFactory::createOne(['owner' => $owner]);
+        $item = ItemFactory::createOne(['collection' => $collection, 'owner' => $owner]);
+        $loan = LoanFactory::createOne(['item' => $item, 'owner' => $owner]);
+
+        // Act
+        $this->createClientWithCredentials($user)->request('PATCH', '/api/loans/'.$loan->getId(), [
+            'headers' => ['Content-Type: application/merge-patch+json'],
+            'json' => [
+                'lentTo' => 'Someone else',
+            ],
+        ]);
+
+        // Assert
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+
+    public function test_cant_delete_another_user_loan(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne()->object();
+        $owner = UserFactory::createOne()->object();
+        $collection = CollectionFactory::createOne(['owner' => $owner]);
+        $item = ItemFactory::createOne(['collection' => $collection, 'owner' => $owner]);
+        $loan = LoanFactory::createOne(['item' => $item, 'owner' => $owner]);
+
+        // Act
+        $this->createClientWithCredentials($user)->request('DELETE', '/api/loans/'.$loan->getId());
+
+        // Assert
+        $this->assertResponseStatusCodeSame(Response::HTTP_NOT_FOUND);
+    }
+}
