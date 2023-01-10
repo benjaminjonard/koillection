d4ebd44015cd536bc9da9518def9478c8f63c08d
benjaminjonard <benjamin.jonard@sooyoos.com>
2023-01-10 13:59:55 +0100

    Add German language

--- src/Enum/LocaleEnum.php
+++ src/Enum/LocaleEnum.php
@@ -12,10 +12,13 @@ class LocaleEnum
 
     public const LOCALE_ES = 'es';
 
+    public const LOCALE_DE = 'de';
+
     public const LOCALES = [
         self::LOCALE_EN => 'en',
         self::LOCALE_FR => 'fr',
         self::LOCALE_ES => 'es',
+        self::LOCALE_DE => 'de',
     ];
 
     public static function getLocaleLabels(): array
@@ -24,6 +27,7 @@ class LocaleEnum
             self::LOCALE_EN => 'global.locale.en',
             self::LOCALE_FR => 'global.locale.fr',
             self::LOCALE_ES => 'global.locale.es',
+            self::LOCALE_DE => 'global.locale.de',
         ];
     }
 }
