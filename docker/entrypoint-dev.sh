4fdbb5589f0836ede8a62c8c4e81d552aeadaa73
benjaminjonard <jonard.benjamin@protonmail.com>
2023-03-17 19:36:38 +0100

    Add missing docker files

---
+++ docker/entrypoint-dev.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+echo "**** Inject .env values ****"
+rm -rf /var/www/koillection/.env.local
+touch /var/www/koillection/.env.local
+
+echo "APP_ENV=${APP_ENV:-prod}" >> "/var/www/koillection/.env.local"
+echo "APP_DEBUG=${APP_DEBUG:-0}" >> "/var/www/koillection/.env.local"
+echo "APP_SECRET=${APP_SECRET:-$(openssl rand -base64 21)}" >> "/var/www/koillection/.env.local"
+
+echo "JWT_SECRET_KEY=${JWT_SECRET_KEY:-%kernel.project_dir%/config/jwt/private.pem}" >> "/var/www/koillection/.env.local"
+echo "JWT_PUBLIC_KEY=${JWT_PUBLIC_KEY:-%kernel.project_dir%/config/jwt/public.pem}" >> "/var/www/koillection/.env.local"
+echo "JWT_PASSPHRASE=${JWT_PASSPHRASE:-$(openssl rand -base64 21)}" >> "/var/www/koillection/.env.local"
+
+echo "DB_DRIVER=${DB_DRIVER:-}" >> "/var/www/koillection/.env.local"
+echo "DB_NAME=${DB_NAME:-}" >> "/var/www/koillection/.env.local"
+echo "DB_HOST=${DB_HOST:-}" >> "/var/www/koillection/.env.local"
+echo "DB_PORT=${DB_PORT:-}" >> "/var/www/koillection/.env.local"
+echo "DB_USER=${DB_USER:-}" >> "/var/www/koillection/.env.local"
+echo "DB_PASSWORD=${DB_PASSWORD:-}" >> "/var/www/koillection/.env.local"
+echo "DB_VERSION=${DB_VERSION:-}" >> "/var/www/koillection/.env.local"
+
+echo "CORS_ALLOW_ORIGIN=${CORS_ALLOW_ORIGIN:-'^https?://(localhost|127\.0\.0\.1)(:[0-9]+)?$'}" >> "/var/www/koillection/.env.local"
+
+echo "session.cookie_secure=${HTTPS_ENABLED}" >> /etc/php/8.2/fpm/conf.d/php.ini
+echo "date.timezone=${PHP_TZ}" >> /etc/php/8.2/fpm/conf.d/php.ini
+
+echo "**** Migrate the database ****"
+cd /var/www/koillection && \
+php bin/console doctrine:migration:migrate --no-interaction --allow-no-migration --env=prod
+
+echo "**** Create nginx log files ****"
+mkdir -p /logs/nginx
+chown -R www-data:www-data /logs/nginx
+
+echo "**** Setup complete, starting the server. ****"
+php-fpm8.2
+exec $@
