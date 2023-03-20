369a446913dc4835ade59e537414f0f4eda39717
benjaminjonard <jonard.benjamin@protonmail.com>
2023-03-20 22:38:29 +0100

    Add some PHP settings as env variables

--- docker/entrypoint-dev.sh
+++ docker/entrypoint-dev.sh
@@ -26,6 +26,11 @@ echo "CORS_ALLOW_ORIGIN=${CORS_ALLOW_ORIGIN:-'^https?://(localhost|127\.0\.0\.1)
 
 echo "session.cookie_secure=${HTTPS_ENABLED}" >> /etc/php/8.2/fpm/conf.d/php.ini
 echo "date.timezone=${PHP_TZ}" >> /etc/php/8.2/fpm/conf.d/php.ini
+echo "memory_limit=${PHP_MEMORY_LIMIT:-'512M'}" >> /etc/php/8.2/fpm/conf.d/php.ini
+
+echo "upload_max_filesize=${UPLOAD_MAX_FILESIZE:-'20M'}" >> /etc/php/8.2/fpm/conf.d/php.ini
+echo "post_max_size=${UPLOAD_MAX_FILESIZE:-'100M'}" >> /etc/php/8.2/fpm/conf.d/php.ini
+sed -i "s/client_max_body_size 100M;/client_max_body_size ${UPLOAD_MAX_FILESIZE:-'100M'};/g" /etc/nginx/nginx.conf
 
 echo "**** Migrate the database ****"
 cd /var/www/koillection && \
