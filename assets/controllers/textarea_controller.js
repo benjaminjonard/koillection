a0542022072d89c963126c96c590baf0213480ec
benjaminjonard <benjamin.jonard@sooyoos.com>
2023-05-24 11:17:45 +0200

    Fix textarea + continue rework

--- assets/controllers/textarea_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@hotwired/stimulus';
-import { M } from '@materializecss/materialize';
-
-export default class extends Controller {
-    connect() {
-        M.textareaAutoResize(this.element);
-    }
-}
