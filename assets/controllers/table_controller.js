ac06aa913fe974e6b055bc8ec16e0d47a50d6d1e
benjaminjonard <benjamin.jonard@sooyoos.com>
2023-08-21 18:04:14 +0200

    Use Collation for ordering in javascript

--- assets/controllers/table_controller.js
+++ assets/controllers/table_controller.js
@@ -1,12 +1,21 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['row', 'sorter']
+    static targets = ['sorter']
 
-    sortDesc(event) {
+    sort(event) {
         const column = event.target.dataset.column;
-        const values = this.element.querySelectorAll(`tbody td[data-column="${column}"]`);
-        const orderedValues = Array.from(values).sort((a, b) => (a.dataset.value > b.dataset.value) ? 1 : -1)
+        const direction = event.target.dataset.direction;
+        const values = this.element.querySelectorAll(`tbody td[data-column="${column}"] span[data-value]`);
+
+        let collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+        let orderedValues;
+
+        if (direction === 'asc') {
+            orderedValues = Array.from(values).sort((a, b) => collator.compare(a.dataset.value, b.dataset.value))
+        } else {
+            orderedValues = Array.from(values).sort((a, b) => collator.compare(b.dataset.value, a.dataset.value))
+        }
 
         orderedValues.forEach((value, index) => {
             this.element.querySelector('tbody').appendChild(value.closest('tr'));
@@ -14,22 +23,6 @@ export default class extends Controller {
 
         this.sorterTargets.forEach(sorter => {
            sorter.classList.remove('active');
-        });
-        event.target.classList.add('active')
-    }
-
-    sortAsc(event) {
-        const column = event.target.dataset.column;
-        const values = this.element.querySelectorAll(`tbody td[data-column="${column}"]`);
-        const orderedValues = Array.from(values).sort((a, b) => (a.dataset.value <= b.dataset.value) ? 1 : -1)
-
-        this.element.querySelector('tbody').innerHTML = '';
-        orderedValues.forEach((value, index) => {
-            this.element.querySelector('tbody').appendChild(value.closest('tr'));
-        });
-
-        this.sorterTargets.forEach(sorter => {
-            sorter.classList.remove('active');
         });
         event.target.classList.add('active')
     }
