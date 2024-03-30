75cc2dc4d37789e7e9ffd75b8fea465bdb7bccb5
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2024-03-30 19:59:09 +0100

    Temp fix for error handler in test suite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require \dirname(__DIR__) . '/vendor/autoload.php';
+
+set_exception_handler([new ErrorHandler(), 'handleException']);
 
 if (file_exists(\dirname(__DIR__) . '/config/bootstrap.php')) {
     require \dirname(__DIR__) . '/config/bootstrap.php';
