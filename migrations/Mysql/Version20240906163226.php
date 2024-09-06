829b9fd4686e73b1950db7aaa42d208c9aa64f47
Benjamin Jonard <jonard.benjamin@protonmail.com>
2024-09-06 20:06:07 +0200

    Fix mysql migration

--- migrations/Mysql/Version20240906163226.php
+++ migrations/Mysql/Version20240906163226.php
@@ -4,28 +4,31 @@ declare(strict_types=1);
 
 namespace App\Migrations\Mysql;
 
+use App\Enum\DisplayModeEnum;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240906163226 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return '[Mysql] Add `search_results_display_mode` to `koi_user`.';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE koi_user ADD search_results_display_mode VARCHAR(255) NOT NULL');
+        $this->skipIf(!$this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform, 'Migration can only be executed safely on \'postgresql\'.');
+
+        $gridDisplayMode = DisplayModeEnum::DISPLAY_MODE_GRID;
+
+        $this->addSql('ALTER TABLE koi_user ADD search_results_display_mode VARCHAR(255)');
+        $this->addSql("UPDATE koi_user SET search_results_display_mode = '{$gridDisplayMode}'");
+        $this->addSql('ALTER TABLE koi_user CHANGE search_results_display_mode search_results_display_mode VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE koi_user DROP search_results_display_mode');
+        $this->skipIf(true, 'Always move forward.');
     }
 }
